0cef9d3e72170cb14d23ee382c05d687f33b808c
jovany-wang <kingchin1218@126.com>
2019-11-30 16:49:57 +0800

    Fix

--- example/src/main/java/org/dst/drpc/async/Client.java
+++ example/src/main/java/org/dst/drpc/async/Client.java
@@ -1,6 +1,7 @@
 package org.dst.drpc.async;
 
 
+import java.sql.Ref;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.atomic.LongAdder;
 import org.dst.drpc.Reference;
@@ -8,13 +9,19 @@ import org.dst.drpc.Reference;
 
 public class Client {
 
-  public static void main(String[] args) throws Throwable {
+  public static void main(String[] args) {
     Reference<IServer> reference = new Reference<>();
     reference.setAddress("dst://127.0.0.1:8080");
     reference.setInterfaceClass(IServer.class);
 
     IServer server = reference.getReference();
     System.out.println(server.say());
+
+    Reference<IServer2> reference2 = new Reference<>();
+    reference2.setAddress("dst://127.0.0.1:8080");
+    reference2.setInterfaceClass(IServer2.class);
+    IServer2 server2 = reference2.getReference();
+    System.out.println(server2.say2());
 
     LongAdder totalCost = new LongAdder();
     for(int i = 0;i<1000;i++) {
