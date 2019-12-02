0d16d0d1480aa60484104c1927c76dc2536bb0f3
senyer <576085903@qq.com>
2019-12-02 23:33:43 +0800

    Fix

--- example/src/main/java/com/distkv/drpc/pb/PBServiceImpl.java
+++ example/src/main/java/com/distkv/drpc/pb/PBServiceImpl.java
@@ -4,7 +4,6 @@ import com.distkv.drpc.pb.generated.CommonProtocol;
 import com.distkv.drpc.pb.generated.StringProtocol;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
