792aaa29735af862d2dbd3975009d7f460e83772
zhengrenjie <1354334940@qq.com>
2019-12-11 18:32:03 +0800

    Protobuf description of request/response for RPC-transport. (#47)
    * pb
    
    * protobuf 3
    
    * error
    
    * move proto; add test module
    
    * up to pb3; add pb test;
    
    * fix pb3 bug;
    
    * fix
    
    * fix

--- transport/src/main/java/com/distkv/drpc/codec/ProtoBufSerialization.java
+++ transport/src/main/java/com/distkv/drpc/codec/ProtoBufSerialization.java
@@ -4,7 +4,7 @@ import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.serializer.JSONSerializer;
 import com.alibaba.fastjson.serializer.SerializeWriter;
 import com.alibaba.fastjson.serializer.SerializerFeature;
-import com.google.protobuf.GeneratedMessage.Builder;
+import com.google.protobuf.GeneratedMessageV3.Builder;
 import com.google.protobuf.Message;
 import java.io.IOException;
 import java.lang.reflect.Method;
