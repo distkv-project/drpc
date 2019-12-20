9d19c71c34bf63fa7d54d24d9badfa434844f523
zrj <zhengrenjie@nowcoder.com>
2019-12-20 15:36:36 +0800

    math.abs

--- transport/src/main/java/com/distkv/drpc/api/worker/HashableChooserFactory.java
+++ transport/src/main/java/com/distkv/drpc/api/worker/HashableChooserFactory.java
@@ -72,7 +72,7 @@ public class HashableChooserFactory implements EventExecutorChooserFactory {
 
     @Override
     public EventExecutor next(int hash) {
-      return executors[(hash < 0 ? -hash : hash) % size];
+      return executors[Math.abs(hash) % size];
     }
   }
 }
