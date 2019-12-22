0b187946c144c8c3a6d186ff2e3afcdeef76d14d
kairbon <wangkai31415926@gmail.com>
2019-12-22 11:58:07 +0800

    fix

--- example/src/main/java/com/distkv/drpc/pb/PBServiceImpl.java
+++ example/src/main/java/com/distkv/drpc/pb/PBServiceImpl.java
@@ -5,8 +5,6 @@ import com.distkv.drpc.pb.generated.StringProtocol;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class PBServiceImpl implements IPBService {
 
