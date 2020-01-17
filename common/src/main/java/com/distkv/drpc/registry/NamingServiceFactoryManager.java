961a6214f3e2571ddfbd5bf714609847f1e163b1
lcm <1137909852@qq.com>
2020-01-17 17:03:40 +0800

    WIP

---
+++ common/src/main/java/com/distkv/drpc/registry/NamingServiceFactoryManager.java
@@ -0,0 +1,47 @@
+package registry;
+
+import java.util.HashMap;
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NamingServiceFactoryManager {
+
+  private Logger logger = LoggerFactory.getLogger(NamingServiceFactoryManager.class);
+
+  /**
+   * FactoryManager thread consistence;
+   */
+  private volatile NamingServiceFactoryManager instance;
+
+  private Map<String, NamingServiceFactory> namingServiceFactoryMap;
+
+  private NamingServiceFactoryManager getInstance() {
+    if (instance == null) {
+      synchronized (NamingServiceFactoryManager.class) {
+        if(instance == null) {
+          instance = new NamingServiceFactoryManager();
+        }
+        return instance;
+      }
+    }
+    return instance;
+  }
+
+  private NamingServiceFactoryManager() {
+    this.namingServiceFactoryMap = new HashMap<>();
+    this.namingServiceFactoryMap.put("dst", new DstNamingServiceFactory());
+  }
+
+  public void registerNamingServiceFactory(NamingServiceFactory factory) {
+    if (namingServiceFactoryMap.get(factory.getName()) != null) {
+      throw new RuntimeException("NamingServiceFactory exists: " + factory.getName());
+    }
+    this.namingServiceFactoryMap.put(factory.getName(), factory);
+    logger.info("ServiceFactory has registered: " + factory.getName());
+  }
+
+  public NamingServiceFactory getNamingServiceFactory(String name) {
+    return this.namingServiceFactoryMap.get(name);
+  }
+}
