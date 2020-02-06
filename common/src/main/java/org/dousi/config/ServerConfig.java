798249dadd6d5bdf4357353baea1107366ee3022
kairbon <45480357+kairbon@users.noreply.github.com>
2020-02-06 11:52:02 +0800

    Change defalut IOThreadOnly flag to false (#101)

--- common/src/main/java/org/dousi/config/ServerConfig.java
+++ common/src/main/java/org/dousi/config/ServerConfig.java
@@ -12,7 +12,7 @@ public class ServerConfig {
   private int workerThreadNum;
 
   @Builder.Default
-  private boolean enableIOThreadOnly = true;
+  private boolean enableIOThreadOnly = false;
 
   /**
    * If every request from the same TCP should be executed by order, set this option true.
