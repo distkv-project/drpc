08a1134d50f39568f1849c6c24ff66a156ab5029
梅杰 <meijie.work@gmail.com>
2020-03-01 00:04:16 +0800

    Add close method for Dousi server. (#111)
    * Dousi Server add close method.
    
    * fix

--- protocol/src/main/java/org/dousi/DousiServer.java
+++ protocol/src/main/java/org/dousi/DousiServer.java
@@ -4,6 +4,9 @@ import org.dousi.api.Handler;
 import org.dousi.api.Server;
 import org.dousi.config.ServerConfig;
 import org.dousi.netty.NettyTransportFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.concurrent.CopyOnWriteArrayList;
 
 
@@ -12,6 +15,9 @@ import java.util.concurrent.CopyOnWriteArrayList;
  */
 public class DousiServer {
 
+  private static Logger logger = LoggerFactory.getLogger(DousiServer.class);
+
+  private Server server;
   /**
    * The service handlers.
    * Note that CopyOnWriteArrayList might not be efficient too much when writing,
@@ -36,7 +42,16 @@ public class DousiServer {
   }
 
   public void run() {
-    Server server = NettyTransportFactory.getInstance().createServer(serverConfig, serviceHandlers);
+    if (server != null) {
+      logger.debug("server is running");
+    }
+    server = NettyTransportFactory.getInstance().createServer(serverConfig, serviceHandlers);
     server.open();
+  }
+
+  public void stop() {
+    if (server != null) {
+      server.close();
+    }
   }
 }
