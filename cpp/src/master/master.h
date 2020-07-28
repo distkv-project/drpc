6bb5119b640660b911573cb0193795a0d25787d6
Qing Wang <kingchin1218@126.com>
2020-07-28 15:13:36 +0800

    WIP

--- cpp/src/master/master.h
+++ cpp/src/master/master.h
@@ -9,12 +9,27 @@
 namespace dousi {
 namespace master {
 
+/**
+ * The master is a standalone component that provides the service discovery for
+ * the Dousi servers and clients.
+ *
+ * A Dousi server provided by user should be registered into master, so that the
+ * client which wants to use this service can find the service location from master.
+ */
 class Master {
 public:
+  Master() = delete;
+
+  Master(std::string listening_host, int16_t listening_port)
+    : listening_host_(std::move(listening_host)),
+    listening_port_(listening_port) {}
 
   void loop() {}
 
 private:
+  std::string listening_host_;
+  int16_t listening_port_;
+
   // The map that maps service name to its endpoint.
   std::unordered_map<std::string, Endpoint> endpoints_;
 };
--- cpp/src/master/master_main.cc
+++ cpp/src/master/master_main.cc
@@ -4,11 +4,16 @@
 
 using namespace dousi;
 
-// The main entrance of master process.
+/**
+ * The main entrance of master process.
+ *
+ * Usage:
+ *   master_main --host=127.0.0.1 --port=9999
+ */
 int main(int argc, char *argv[]) {
 
   std::cout << "Master Main..." << std::endl;
-  master::Master master;
+  master::Master master("", 0);
   master.loop();
 
   return 0;
